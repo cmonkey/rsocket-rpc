e4f30fde727f617286594ba1721ffeec37f3349f
CMonkey <42.codemonkey@gmail.com>
2019-04-01 10:57:20 +0800

    add RsocketRpcClientApplication

---
+++ src/main/scala/org/excavator/rsocket/rpc/RsocketRpcClientApplication.scala
@@ -0,0 +1,9 @@
+package org.excavator.rsocket.rpc
+
+import org.slf4j.LoggerFactory
+
+class RsocketRpcClientApplication(host: String, port: Int) {
+
+  val logger = LoggerFactory.getLogger(classOf[RsocketRpcClientApplication])
+
+}
