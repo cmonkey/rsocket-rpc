7f486d1013e911581afc9d8e8014d1a3a061fc08
CMonkey <42.codemonkey@gmail.com>
2019-04-01 14:42:07 +0800

    add RsocketRpcClientApplication .streamingRequestSingleResponse

--- src/main/scala/org/excavator/rsocket/rpc/RsocketRpcClientApplication.scala
+++ src/main/scala/org/excavator/rsocket/rpc/RsocketRpcClientApplication.scala
@@ -1,6 +1,6 @@
 package org.excavator.rsocket.rpc
 
-import io.rsocket.RSocketFactory
+import io.rsocket.{RSocket, RSocketFactory}
 import io.rsocket.transport.netty.client.TcpClientTransport
 import org.excavator.grpc.rsocket.rpc.{SimpleRequest, SimpleServiceClient}
 import org.slf4j.LoggerFactory
@@ -10,11 +10,16 @@ class RsocketRpcClientApplication {
 
   val logger = LoggerFactory.getLogger(classOf[RsocketRpcClientApplication])
 
+  var rSocket: RSocket = null
+
   def connect(port: Int) = {
-    val rSocket = RSocketFactory.connect()
+    rSocket = RSocketFactory.connect()
       .transport(TcpClientTransport.create(port))
       .start()
       .block()
+  }
+
+  def streamingRequestSingleResponse() = {
 
     val serviceClient = new SimpleServiceClient(rSocket)
 
@@ -22,6 +27,6 @@ class RsocketRpcClientApplication {
 
     val response = serviceClient.streamingRequestSingleResponse(requests, io.netty.buffer.Unpooled.EMPTY_BUFFER).block
 
-    logger.info(s"response = ${response}")
+    response
   }
 }
