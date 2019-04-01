f0b00e9eb1eb48f68423d1d0e92e4d0519e83481
CMonkey <42.codemonkey@gmail.com>
2019-04-01 12:09:49 +0800

    add RsocketRpcClientApplication.connect

--- src/main/scala/org/excavator/rsocket/rpc/RsocketRpcClientApplication.scala
+++ src/main/scala/org/excavator/rsocket/rpc/RsocketRpcClientApplication.scala
@@ -1,9 +1,32 @@
 package org.excavator.rsocket.rpc
 
+import io.rsocket.RSocketFactory
+import io.rsocket.transport.netty.client.TcpClientTransport
+import org.excavator.grpc.rsocket.rpc.{SimpleRequest, SimpleServiceClient}
 import org.slf4j.LoggerFactory
+import reactor.core.publisher.Flux
 
 class RsocketRpcClientApplication(host: String, port: Int) {
 
   val logger = LoggerFactory.getLogger(classOf[RsocketRpcClientApplication])
 
+  def connect(port: Int) = {
+    val rSocket = RSocketFactory.connect()
+      .transport(TcpClientTransport.create(port))
+      .start()
+      .block()
+
+    val serviceClient = new SimpleServiceClient(rSocket)
+
+    val requests:Flux[SimpleRequest] = Flux.range(1, 11)
+      .map(i => "sending -> " + i)
+      .map(s => {
+        logger.info(s"s = ${s}")
+        SimpleRequest.newBuilder().setRequestMessage(s.getRequestMessage).build()
+      })
+
+    val response = serviceClient.streamingRequestSingleResponse(requests, io.netty.buffer.Unpooled.EMPTY_BUFFER).block
+
+    logger.info(s"response = ${response}")
+  }
 }
